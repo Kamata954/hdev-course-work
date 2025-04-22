009ec558f5d847c63bdd0e7f30ea4d74cfc7280a
Kamata954 <kimani.walker954@gmail.com>
2025-04-22 09:45:40 -0500

    async practice

--- class35-materials/class35-materials/api-template/js/main.js
+++ class35-materials/class35-materials/api-template/js/main.js
@@ -1,1 +1,19 @@
 //Make an api request using async await
+
+document.querySelector("button").addEventListener('click', getImage);
+
+async function getImage() {
+    try{
+        const res = await fetch("https://api.waifu.im/search?included_tags=maid");
+        if(!res.ok){
+            throw new Error(`Response status: ${res.status}`);
+        }
+        const data = await res.json();
+        console.log(data);
+
+        document.querySelector("img").src = data.images[0].url;
+    } catch (error) {
+        console.error(error.message)
+    }
+
+}
